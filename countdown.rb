8a4039bb2cdf7225c18f7378b9e63ce501369de5
Bernadette Davis <bsd231@nyu.edu>
2019-05-14 20:04:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,13 @@ def countdown(n)
   return 'HAPPY NEW YEAR!'
 
 end
+
+def countdown_with_sleep(n)
+  sleep(5)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n = n - 1
+  end
+
+  return 'HAPPY NEW YEAR!'
+end
